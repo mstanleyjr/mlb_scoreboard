d8e329133e6b0a79c8e99c96e3c22ff5c74778eb
Stanley <mjstanleyjr@gmail.com>
2025-11-21 18:43:37 -0500

    Main

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+
+	println("Starting MLB Scoreboard")
+	//
+	//os.Setenv("MATRIX_EMULATOR", "1")
+	//config := &rgbmatrix.DefaultConfig
+	//println("%v", config)
+	//m, _ := rgbmatrix.NewRGBLedMatrix(config)
+	////
+	//c := rgbmatrix.NewCanvas(m)
+	//defer c.Close()
+	//
+	//draw.Draw(c, c.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
+	//c.Render()
+	//
+	//time.Sleep(45 * time.Second)
+
+	// Yeah I just gotta do this on the other laptop so I can emulate
+
+	// make a config object
+	// make a client
+	// Should I bother with tests here?
+	// Future me might like it and they won't be that expensive to mock
+	// then if I put this up for future stuff it won't be a tragedy.
+
+}
