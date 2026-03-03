680a1adc1a7ed45a76591d4cf04f62cc5f06b191
stanley <mjstanleyjr@gmail.com>
2026-03-02 22:45:09 -0500

    Test emulator on pi

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ import (
 )
 
 func main() {
-	_, _ = context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	_ = ctx // Will be used when goroutines are enabled
 
 	controller := &scoreboard.DisplayController{}
 	controller.Cond = sync.NewCond(&controller.Mu)
@@ -31,21 +33,49 @@ func main() {
 
 	//wg.Wait()
 
-	fmt.Println("HERE WE GO`")
-	err := os.Setenv("MATRIX_EMULATOR", "1")
-	if err != nil {
-		panic(err)
+	fmt.Println("HERE WE GO")
+
+	// Check if we should use the matrix emulator or skip it for development
+	useEmulator := os.Getenv("USE_MATRIX_EMULATOR") == "1"
+
+	if useEmulator {
+		// Check if DISPLAY is set (required for X11 emulator)
+		if os.Getenv("DISPLAY") == "" {
+			fmt.Println("Warning: USE_MATRIX_EMULATOR is set but DISPLAY is not configured")
+			fmt.Println()
+			fmt.Println("For SSH connections, you have two options:")
+			fmt.Println("  1. Enable X11 forwarding when connecting:")
+			fmt.Println("     ssh -X user@host")
+			fmt.Println("     Then run: USE_MATRIX_EMULATOR=1 go run main.go")
+			fmt.Println()
+			fmt.Println("  2. Run without the emulator (recommended for headless servers):")
+			fmt.Println("     go run main.go")
+			fmt.Println()
+			fmt.Println("Skipping matrix emulator initialization for this run")
+		} else {
+			err := os.Setenv("MATRIX_EMULATOR", "1")
+			if err != nil {
+				panic(err)
+			}
+
+			m, _ := rgbmatrix.NewRGBLedMatrix(&rgbmatrix.DefaultConfig)
+			c := rgbmatrix.NewCanvas(m)
+			defer func() {
+				_ = c.Close()
+			}()
+
+			draw.Draw(c, c.Bounds(), &image.Uniform{color.White}, image.Point{}, draw.Src)
+
+			if err := c.Render(); err != nil {
+				fmt.Printf("Error rendering canvas: %v\n", err)
+			}
+
+			time.Sleep(30 * time.Second)
+		}
+	} else {
+		fmt.Println("Running in development mode without matrix hardware")
+		fmt.Println("To enable the LED matrix emulator, use: USE_MATRIX_EMULATOR=1 go run main.go")
 	}
-
-	m, _ := rgbmatrix.NewRGBLedMatrix(&rgbmatrix.DefaultConfig)
-	c := rgbmatrix.NewCanvas(m)
-	defer c.Close()
-
-	draw.Draw(c, c.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
-
-	c.Render()
-
-	time.Sleep(30 * time.Second)
 
 	//
 	// make a config object
