65b0986e4b1017b7ccae1d81282ba0090538a972
stanley <mjstanleyjr@gmail.com>
2026-03-26 23:40:12 -0400

    Display test

--- main.go
+++ main.go
@@ -1,47 +1,58 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
+	"image"
+	"image/color"
+	"image/draw"
 	"sync"
 	"time"
 
-	//rgbmatrix "github.com/mcuadros/go-rpi-rgb-led-matrix"
+	rgbmatrix "github.com/mcuadros/go-rpi-rgb-led-matrix"
 	"github.com/mstanleyjr/mlb_scoreboard/scoreboard"
 )
 
 func main() {
-	ctx, _ := context.WithCancel(context.Background())
+	//ctx, _ := context.WithCancel(context.Background())
 
 	controller := &scoreboard.DisplayController{}
 	controller.Cond = sync.NewCond(&controller.Mu)
 	controller.Paused = false
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go scoreboard.StartScoreboard(ctx, &wg, controller)
-	//wg.Add(1)
-	//go radio.StartRadio(ctx, &wg, controller)
-	// This would be where I do the radio listener to check the button
+	//var wg sync.WaitGroup
+	////wg.Add(1)
+	////go scoreboard.StartScoreboard(ctx, &wg, controller)
+	////wg.Add(1)
+	////go radio.StartRadio(ctx, &wg, controller)
+	//// This would be where I do the radio listener to check the button
+	//
+	//wg.Wait()
+	//
+	//fmt.Println("HERE WE GO`")
+	//err := os.Setenv("MATRIX_EMULATOR", "1")
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	wg.Wait()
+	m, _ := rgbmatrix.NewRGBLedMatrix(&rgbmatrix.DefaultConfig)
+	c := rgbmatrix.NewCanvas(m)
+	defer func(c *rgbmatrix.Canvas) {
+		err := c.Close()
+		if err != nil {
+			fmt.Println("Error closing canvas")
+			return
+		}
+	}(c)
 
-	fmt.Println("HERE WE GO`")
-	err := os.Setenv("MATRIX_EMULATOR", "1")
+	draw.Draw(c, c.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
+
+	err := c.Render()
 	if err != nil {
-		panic(err)
+		fmt.Println("Error rendering canvas")
+		return
 	}
 
-	//m, _ := rgbmatrix.NewRGBLedMatrix(&rgbmatrix.DefaultConfig)
-	//c := rgbmatrix.NewCanvas(m)
-	//defer c.Close()
-	//
-	//draw.Draw(c, c.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
-	//
-	//c.Render()
-
-	time.Sleep(30 * time.Second)
+	time.Sleep(15 * time.Second)
 
 	//
 	// make a config object
