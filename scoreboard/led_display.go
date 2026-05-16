6581a73b560a9384130dc24b2e4b1cf4a98f9a20
stanley <mjstanleyjr@gmail.com>
2026-05-15 22:50:45 -0400

    Update division standings title gap for improved layout

--- scoreboard/led_display.go
+++ scoreboard/led_display.go
@@ -11,7 +11,7 @@ import (
 const (
 	divisionStandingsTopPadding    = 2
 	divisionStandingsTitleH        = 8
-	divisionStandingsTitleGap      = 5
+	divisionStandingsTitleGap      = 8
 	divisionStandingsTitleRuleGap  = 1
 	divisionStandingsRowBlockH     = 19
 	divisionStandingsRankX         = 1
