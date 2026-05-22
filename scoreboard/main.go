311db1d4fa8face3e29aa2d03887e08ee1718665
stanley <mjstanleyjr@gmail.com>
2026-05-21 22:01:06 -0400

    Get back to whole

--- scoreboard/main.go
+++ scoreboard/main.go
@@ -96,7 +96,7 @@ func StartScoreboard(ctx context.Context, wg *sync.WaitGroup, controller *Displa
 
 	pages := make([]func(scoreboardInfo ScoreboardInformation), 0)
 	pages = append(pages, func(scoreboardInfo ScoreboardInformation) {
-		//StandingsRollupDisplay(ctx, scoreboardInfo, controller, standingsRollupMaxDuration)
+		StandingsRollupDisplay(ctx, scoreboardInfo, controller, standingsRollupMaxDuration)
 	})
 	pages = append(pages, func(scoreboardInfo ScoreboardInformation) {
 		NextMatchupDisplay(ctx, scoreboardInfo, mlbClient, controller)
